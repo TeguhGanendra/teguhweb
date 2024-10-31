1f05e5b7b7080e7edefef5bf667a3ba967a217af
Teguh Ganendra <teguh.septiawan.ts@gmail.com>
2024-10-31 20:03:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Website loaded!');
+// script.js
+let lastScroll = 0;
+const header = document.querySelector('.sticky-header');
+
+// Menampilkan dan menyembunyikan header berdasarkan scroll
+window.addEventListener('scroll', () => {
+    const currentScroll = window.pageYOffset;
+
+    if (currentScroll > lastScroll) {
+        header.style.transform = 'translateY(-100%)'; // Menyembunyikan header
+    } else {
+        header.style.transform = 'translateY(0)'; // Menampilkan header
+    }
+    lastScroll = currentScroll; // Memperbarui nilai scroll terakhir
 });
